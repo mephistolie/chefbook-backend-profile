d8ce551d7a8150162cda4a2721e975f2a1f47b4e
Mikhail Levin <mikhaillevin@inbox.ru>
2024-09-07 14:28:09 +0300

    feature: update visible name logic

--- internal/service/profile_min_info.go
+++ internal/service/profile_min_info.go
@@ -2,10 +2,12 @@ package service
 
 import (
 	"context"
+	"fmt"
 	auth "github.com/mephistolie/chefbook-backend-auth/api/proto/implementation/v1"
 	"github.com/mephistolie/chefbook-backend-common/log"
 	"github.com/mephistolie/chefbook-backend-profile/internal/entity"
 	user "github.com/mephistolie/chefbook-backend-user/api/proto/implementation/v1"
+	"strings"
 	"sync"
 	"time"
 )
@@ -13,59 +15,64 @@ import (
 func (s *Service) GetProfilesMinInfo(profileIds []string) (map[string]entity.ProfileMinInfo, error) {
 	wg := sync.WaitGroup{}
 
-	fallbackNames := make(map[string]string)
+	var nicknames map[string]string
+	var users map[string]*user.UserMinInfo
 
 	wg.Add(1)
 	go func() {
-		fallbackNames = s.getProfilesAuthNames(profileIds)
+		nicknames = s.getProfilesAuthNames(profileIds)
 		wg.Done()
 	}()
 
-	infos, hasAllNames := s.getProfilesUserInfo(profileIds)
+	wg.Add(1)
+	go func() {
+		users = s.getProfilesUserInfo(profileIds)
+		wg.Done()
+	}()
 
-	if !hasAllNames {
-		wg.Wait()
-		for id, info := range infos {
-			if info.VisibleName == nil {
-				if fallbackName, ok := fallbackNames[id]; ok {
-					info.VisibleName = &fallbackName
-					infos[id] = info
-				}
-			}
+	infos := make(map[string]entity.ProfileMinInfo)
+	for _, id := range profileIds {
+		fallbackName := id
+		separatorIndex := strings.Index(id, "-")
+		if separatorIndex >= 0 {
+			fallbackName = fmt.Sprintf("#%s", fallbackName[0:separatorIndex])
 		}
+		infos[id] = entity.ProfileMinInfo{
+			Id:          id,
+			VisibleName: &fallbackName,
+		}
+	}
+
+	wg.Wait()
+
+	for id, profile := range users {
+		info := infos[id]
+		if profile.FullName != nil {
+			info.VisibleName = profile.FullName
+		}
+		info.Avatar = profile.Avatar
+		infos[id] = info
+	}
+	for id, nickname := range nicknames {
+		info := infos[id]
+		info.VisibleName = &nickname
+		infos[id] = info
 	}
 
 	return infos, nil
 }
 
-func (s *Service) getProfilesUserInfo(profileIds []string) (map[string]entity.ProfileMinInfo, bool) {
+func (s *Service) getProfilesUserInfo(profileIds []string) map[string]*user.UserMinInfo {
 	ctx, cancelCtx := context.WithTimeout(context.Background(), 2*time.Second)
 	res, err := s.repos.User.GetUsersMinInfo(ctx, &user.GetUsersMinInfoRequest{UserIds: profileIds})
 	cancelCtx()
 
-	requestProfilesCount := len(profileIds)
-	responseProfileCount := len(res.Infos)
-
-	hasAllNames := responseProfileCount >= requestProfilesCount
-
-	infos := make(map[string]entity.ProfileMinInfo)
-	if err == nil {
-		for id, dto := range res.Infos {
-			info := entity.ProfileMinInfo{
-				Id:          id,
-				VisibleName: dto.FullName,
-				Avatar:      dto.Avatar,
-			}
-			if info.VisibleName == nil {
-				hasAllNames = false
-			}
-			infos[id] = info
-		}
-	} else {
+	if err != nil {
 		log.Warnf("unable to get profiles minimal info")
+		return make(map[string]*user.UserMinInfo)
 	}
 
-	return infos, hasAllNames
+	return res.Infos
 }
 
 func (s *Service) getProfilesAuthNames(profileIds []string) map[string]string {
@@ -73,15 +80,10 @@ func (s *Service) getProfilesAuthNames(profileIds []string) map[string]string {
 	res, err := s.repos.Auth.GetVisibleNames(ctx, &auth.GetVisibleNamesRequest{UserIds: profileIds})
 	cancelCtx()
 
-	names := make(map[string]string)
-
-	if err == nil {
-		for id, name := range res.UserVisibleNames {
-			names[id] = name
-		}
-	} else {
+	if err != nil {
 		log.Warnf("unable to get profiles auth names")
+		return make(map[string]string)
 	}
 
-	return names
+	return res.UserVisibleNames
 }
